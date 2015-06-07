6a2d3c85bd5ecca9fb96ddb08f6cf731c0841adb
eatmorehufu <hhu.hufu@gmail.com>
2015-06-07 08:48:41 -0400

    add @en_passant rspecs to board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -122,20 +122,39 @@ let(:board) { Board.create_new_board }
 
   describe "#move when en-passanting" do
 
-    it "lets the pawn move into the en-passant space" do
+    let!(:en_passant_board) do
       board.move([1,1], [3,1])
       board.move([3,1], [4,1])
       board.move([6,2], [4,2])
-      board.move([4,1], [3,2])
+    end
+
+    it "lets the pawn move into the en-passant space" do
+      board.move([4,1], [5,2])
       expect(board[[3,2]].class).to eq(Pawn)
     end
 
     it "captures the en-passanted pawn" do
-      board.move([1,1], [3,1])
-      board.move([3,1], [4,1])
-      board.move([6,2], [4,2])
-      board.move([4,1], [3,2])
       expect(board[[3,2]].class).to eq(Pawn)
+    end
+
+  end
+
+  describe "board@en_passant" do
+
+  let!(:en_pas_move) { board.move([6,2], [4,2]) }
+  
+    it "marks the last pawn to move as the en_passant pawn" do
+      expect(board.en_passant).to eq([4,2])
+    end
+
+    it "removes the pawn from en_passant after the next turn" do
+      board.move([4,2], [3,2])
+      expect(board.en_passant).to eq([])
+    end
+
+    it "replaces the pawn if next move is en_passantable pawn" do
+      board.move([1,1], [3,1])
+      expect(board.en_passant). to eq([3,1])
     end
 
   end
--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -2,11 +2,25 @@ require 'requirements'
 
 describe "Pawn" do
 
-  describe "#moves" do
+  let(:board) { Board.create_new_board }
 
-    it "includes the en-passantable move in moves"
-    it "doesn't include en-passant move if pawn moved one space"
+  describe "#moves for en_passant" do
 
+
+    let!(:en_passanting) do
+      board.move([1,1], [3,1])
+      board.move([3,1], [4,1])
+    end
+
+    it "includes the en-passantable move in moves" do
+      board.move([6,2], [4,2])
+      expect(board[[4,1]].moves.include?([5,2])).to eq(true)
+    end
+    it "doesn't include en-passant move if pawn moved one space" do
+      board.move([6,2], [5,2])
+      board.move([5,2], [4,2])
+      expect(board[[4,1]].moves.include?([5,2])).to eq(false)
+    end
   end
 
 end
